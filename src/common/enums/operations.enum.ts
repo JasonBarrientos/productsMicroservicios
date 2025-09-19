226add784880ec18679e6850be3b0eb15ac14a1b
jason <167580826+JasonBarrientos@users.noreply.github.com>
2025-09-19 15:30:47 -0300

    messagepattern

---
+++ src/common/enums/operations.enum.ts
@@ -0,0 +1,7 @@
+export  enum Operations {   
+    CREATE='createProduct',
+    UPDATE='updateProduct',
+    DELETE='deleteProduct',
+    FIND_ALL='findAllProducts',
+    FIND_ONE='findOneProduct'
+}   
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -3,33 +3,36 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dtos';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Operations } from 'src/common/enums';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
+  // @Post()
+  @MessagePattern(Operations.CREATE)
+  create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
-  @Get()
-  findAll(@Query() paginationDto: PaginationDto) {
+  @MessagePattern(Operations.FIND_ALL)
+  findAll(@Payload() paginationDto: PaginationDto) {
     return this.productsService.findAll(paginationDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
+  @MessagePattern(Operations.FIND_ONE)
+  findOne(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id',ParseIntPipe) id: number, @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(id, updateProductDto);
+  @MessagePattern(Operations.UPDATE)
+  update(@Payload('id',ParseIntPipe) id: number, @Payload() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(updateProductDto.id, updateProductDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id',ParseIntPipe) id: number) {
+  @MessagePattern(Operations.DELETE)
+  remove(@Payload('id',ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
 }
