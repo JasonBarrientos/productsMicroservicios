172b1a54fbfc0ec9653f4b178b5a728eb07970b5
jason <167580826+JasonBarrientos@users.noreply.github.com>
2025-09-20 10:54:11 -0300

    archivos prisma

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PrismaClient } from 'generated/prisma';
+import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common/dtos';
 
 @Injectable()
